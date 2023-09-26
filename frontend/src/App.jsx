f52116e823d28f402ffb191acfa2fdfe1f964d9d
Nathan Lufuluabo <45732868+natelufuluabo@users.noreply.github.com>
2023-09-26 19:25:25 -0400

    installed bootstrap & sass for styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,21 @@
-import './App.css'
+import './App.css';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [contacts, setContacts] = useState([]);
+  useEffect(() => {
+    async function fetchAllContacts() {
+      const response = await axios.get("http://localhost:3001/contacts");
+      setContacts(response.data);
+    }
+    fetchAllContacts();
+  }, []);
   return (
     <>
+      <nav>
+        <
+      </nav>
       Hello, world!
     </>
   )
