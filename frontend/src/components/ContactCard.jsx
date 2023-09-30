f0d4b786b3c0fad84182e63f165b0008d908c071
Nathan Lufuluabo <45732868+natelufuluabo@users.noreply.github.com>
2023-09-30 13:04:53 -0400

    added last touch for the contact card

--- frontend/src/components/ContactCard.jsx
+++ frontend/src/components/ContactCard.jsx
@@ -13,13 +13,13 @@ export default function ContactCard({ contact, setContact }) {
                     <a href="#"><i className="fa-solid fa-arrow-left"></i> Contact List</a>
                     <div className="cardBody">
                         <h2 className="cardTitle">Contact Card</h2>
-                        <p className="cardText m-0"><span><strong>First Name:</strong> {contact.first_name}</span> <button><i className="fa-solid fa-pen"></i></button></p>
-                        <p className="cardText m-0"><span><strong>Last Name:</strong> {contact.last_name}</span> <button><i className="fa-solid fa-pen"></i></button></p>
-                        <p className="cardText m-0"><span><strong>Email:</strong> {contact.email}</span> <button><i className="fa-solid fa-pen"></i></button></p>
-                        <p className="cardText m-0"><span><strong>City:</strong> {contact.city}</span> <button><i className="fa-solid fa-pen"></i></button></p>
-                        <p className="cardText m-0"><span><strong>Province:</strong> {contact.province}</span> <button><i className="fa-solid fa-pen"></i></button></p>
-                        <p className="cardText m-0"><span><strong>Groups:</strong> {contact.groups.join(", ")}</span> <button><i className="fa-solid fa-pen"></i></button></p>
-                        <p className="cardText m-0"><span><strong>Phone:</strong> {contact.phone_number}</span> <button><i className="fa-solid fa-pen"></i></button></p>
+                        <p className="cardText m-0"><span><strong>First Name:</strong> {contact.first_name}</span></p>
+                        <p className="cardText m-0"><span><strong>Last Name:</strong> {contact.last_name}</span></p>
+                        <p className="cardText m-0"><span><strong>Email:</strong> {contact.email}</span></p>
+                        <p className="cardText m-0"><span><strong>City:</strong> {contact.city}</span></p>
+                        <p className="cardText m-0"><span><strong>Province:</strong> {contact.province}</span></p>
+                        <p className="cardText m-0"><span><strong>Groups:</strong> {contact.groups.join(", ")}</span></p>
+                        <p className="cardText m-0"><span><strong>Phone:</strong> {contact.phone_number}</span></p>
                         <div className="d-flex align-items-center gap-2">
                             <button className="btn btn-danger">Delete</button>
                             <button onClick={() => setEditing(true)} className="btn btn-info">Edit</button>
