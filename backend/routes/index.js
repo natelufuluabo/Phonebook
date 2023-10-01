1227b2e31cd4b5eb8a984433905a2392fef3af8b
Nathan Lufuluabo <45732868+natelufuluabo@users.noreply.github.com>
2023-10-01 09:22:37 -0400

    built the api route for different request on the server

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+const fetch = require('node-fetch');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+router.get("/info", async function(req, res, next) {
+  const currentTime = new Date();
+  try {
+    const response = await fetch("http://localhost:3000/api/persons");
+    if (!response.ok) {
+      throw new Error(`HTTP Error! Status: ${response.status}`);
+    }
+    const data = await response.json();
+    res.send(`<p>Phone book has info for ${data.length} ${data.length > 1 ? "person" : "people"}</p> <br/> <p>${currentTime}</p>`)
+  } catch (error) {
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 module.exports = router;
