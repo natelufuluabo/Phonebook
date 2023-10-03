3ba2c89df055be9514fdc3b8e69fe128e1bdb989
Nathan Lufuluabo <45732868+natelufuluabo@users.noreply.github.com>
2023-10-03 13:18:27 -0400

    removed the /info and deployed the app with fly.io

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,20 +1,8 @@
 var express = require('express');
 var router = express.Router();
-const fetch = require('node-fetch');
 
-
-router.get("/info", async function(req, res, next) {
-  const currentTime = new Date();
-  try {
-    const response = await fetch("http://localhost:3000/api/persons");
-    if (!response.ok) {
-      throw new Error(`HTTP Error! Status: ${response.status}`);
-    }
-    const data = await response.json();
-    res.send(`<p>Phone book has info for ${data.length} ${data.length > 1 ? "person" : "people"}</p> <br/> <p>${currentTime}</p>`)
-  } catch (error) {
-    res.status(500).send("Internal Server Error");
-  }
+router.get("/", function(req, res) {
+  res.send("<h1>Welcome to PhoneBook API</h1>")
 });
 
 module.exports = router;
