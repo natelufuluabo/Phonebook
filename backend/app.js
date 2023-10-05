4599d96fd1c8fc8c4c814f02a683595af9efce42
Nathan Lufuluabo <45732868+natelufuluabo@users.noreply.github.com>
2023-10-04 20:53:44 -0400

    successfully connected the backend server to the database

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,22 @@
 var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const dotenv = require('dotenv');
 
 var indexRouter = require('./routes/index');
 var apiRouter = require('./routes/api');
 
 var app = express();
 
+dotenv.config();
+
+const mongoose = require('mongoose');
+const url = `mongodb+srv://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@phonebook.yzrrcfv.mongodb.net/PhoneBookApp?retryWrites=true&w=majority`
+mongoose.set('strictQuery',false)
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 app.use(logger('dev'));
 app.use(express.json());
