17ec6c47ae13949904e4c751ba0f973254b48a51
Nathan Lufuluabo <45732868+natelufuluabo@users.noreply.github.com>
2023-10-06 08:11:27 -0400

    moved all my middleware functions into a separate file

--- backend/app.js
+++ backend/app.js
@@ -1,51 +1,33 @@
 /* eslint-disable max-len */
 const express = require('express');
 const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const dotenv = require('dotenv');
 const cors = require('cors');
+const mongoose = require('mongoose');
 const loggerFunc = require('./utils/logger');
 const config = require('./utils/config');
+const middleware = require('./utils/middleware');
 
 const indexRouter = require('./routes/index');
 const apiRouter = require('./routes/api');
 
 const app = express();
 
-dotenv.config();
-app.use(cors());
-
-const mongoose = require('mongoose');
-const url = `${config.MONGODB_URI}`;
-mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true})
+logger.info('connecting to', config.MONGODB_URI);
+mongoose.connect(config.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => loggerFunc.info('Connected to MongoDB'))
     .catch((error) => logger.error('Error connecting to MongoDB:', error.message));
-app.use(logger('dev'));
+
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
+app.use(middleware.requestLogger);
 
 app.use('/', indexRouter);
 app.use('/api', apiRouter);
 
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({error: 'Endpoint does not exist.'});
-};
 
-app.use(unknownEndpoint);
-
-const errorHandler = (error, request, response, next) => {
-  loggerFunc.error(error.message);
-
-  if (error.name === 'CastError') {
-    return response.status(400).send({error: 'malformatted id'});
-  } else if (error.name === 'ValidationError') {
-    return response.status(400).json({error: error.message});
-  }
-
-  next(error);
-};
-
-app.use(errorHandler);
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
 module.exports = app;
