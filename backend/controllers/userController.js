49bd3e41618ced07ff1079172a0b95342ad3c280
Nathan Lufuluabo <45732868+natelufuluabo@users.noreply.github.com>
2023-10-13 07:22:12 -0400

    installed the mongoose-unique-validator to make sure username are unique

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,19 @@
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
 exports.userCreate = async function(req, res) {
-    
-}
+  const {username, name, password} = req.body;
+
+  const saltRounds = 10;
+  const passwordHash = await bcrypt.hash(password, saltRounds);
+
+  const user = new User({
+    username,
+    name,
+    passwordHash,
+  });
+
+  const savedUser = await user.save();
+
+  response.status(201).json(savedUser);
+};
